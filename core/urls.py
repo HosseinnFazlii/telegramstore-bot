d559b962a517d09f0ca090482cd791c52d2bd8df
Hossein <fazlihossein6@gmail.com>
2025-05-10 19:06:29 +0330

    d

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('grappelli/', include('grappelli.urls')),
+    #path('grappelli/', include('grappelli.urls')),
 ]
 
 
