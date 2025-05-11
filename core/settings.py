705a2f8df7bdd3e792c99402c05dfe446b1b1773
Hossein <fazlihossein6@gmail.com>
2025-05-11 10:46:19 +0330

    lets go

--- core/settings.py
+++ core/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['faramarzi.sooxy.xyz']
 # Application definition
 
 INSTALLED_APPS = [
-    'grappelli',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
