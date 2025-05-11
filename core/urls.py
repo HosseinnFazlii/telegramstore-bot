c1c8d4aa63aad4c17b8abca602894472943a12b1
Hossein <fazlihossein6@gmail.com>
2025-05-11 10:49:23 +0330

    lets test jet admin panel

--- core/urls.py
+++ core/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('jet/', include('jet.urls', 'jet')),  # ✅ Django JET dashboard
     path('admin/', admin.site.urls),
     #path('grappelli/', include('grappelli.urls')),
 ]
