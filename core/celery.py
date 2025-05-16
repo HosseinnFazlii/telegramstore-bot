fbc8962be47321a840d63a60955a21a41f9929b7
Hossein <fazlihossein6@gmail.com>
2025-05-16 22:52:00 +0330

    ishalla dg jedi fixe

--- core/celery.py
+++ core/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-from celery import crontab
+from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")  # Update 'core' with your project name
