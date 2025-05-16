6149627cbf40a91d836c760e50df00f6d2641802
Hossein <fazlihossein6@gmail.com>
2025-05-16 21:39:41 +0330

    test somthing

--- store/admin.py
+++ store/admin.py
@@ -42,30 +42,8 @@ class TelegramUserAdmin(admin.ModelAdmin):
 
 from django.contrib import admin
 from .models import ChannelMessage
-from django import forms
-
-class ChannelMessageForm(forms.ModelForm):
-    class Meta:
-        model = ChannelMessage
-        fields = '__all__'
-        widgets = {
-            'scheduled_time': forms.TimeInput(format='%H:%M', attrs={'type': 'time'}),
-            'scheduled_datetime': forms.DateTimeInput(format='%Y-%m-%d %H:%M', attrs={'type': 'datetime-local'}),
-        }
-
-    def clean(self):
-        cleaned_data = super().clean()
-        schedule_type = cleaned_data.get("schedule_type")
-        scheduled_time = cleaned_data.get("scheduled_time")
-        scheduled_datetime = cleaned_data.get("scheduled_datetime")
-
-        if schedule_type == "daily" and not scheduled_time:
-            raise forms.ValidationError("برای حالت روزانه، زمان باید مشخص شود.")
-        if schedule_type == "once" and not scheduled_datetime:
-            raise forms.ValidationError("برای ارسال یک‌باره، تاریخ و زمان باید مشخص شود.")
 
 @admin.register(ChannelMessage)
 class ChannelMessageAdmin(admin.ModelAdmin):
-    form = ChannelMessageForm
     list_display = ['title', 'schedule_type', 'scheduled_time', 'scheduled_datetime', 'sent']
     list_filter = ['schedule_type', 'sent']
