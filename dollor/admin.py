03da9e9b72a815ba9e520c0dfdc55fd8509a001a
Hossein <fazlihossein6@gmail.com>
2025-05-17 11:39:20 +0330

    BORO BERIM

--- dollor/admin.py
+++ dollor/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import DollorPrice  # Adjust the import path if needed
 
-# Register your models here.
+@admin.register(DollorPrice)
+class DollorPriceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'recorded_at')
+    list_filter = ('title', 'recorded_at')
+    search_fields = ('title', 'price', 'description')
+    ordering = ('-recorded_at',)
